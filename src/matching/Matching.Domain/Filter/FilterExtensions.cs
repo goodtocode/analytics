422a372208ca7ac5c545787e4cf00d1245b6eee2
Robert Good <robert.good@goodtocode.com>
2021-09-25 13:23:50 -0700

    Added filter extension

---
+++ src/matching/Matching.Domain/Filter/FilterExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodToCode.Analytics.Matching.Filter
+{
+    public static class FilterExtensions
+    {
+        public static IEnumerable<product> WhereInStock(this IEnumerable<product> p)
+        {
+            return p.Where(x => x.Qty > 0);
+        }
+    }
+}
