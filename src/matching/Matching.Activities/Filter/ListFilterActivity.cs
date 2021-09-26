fdb27c2ec5c2751ab2944bd64e79a484a3b27c91
Robert Good <robert.good@goodtocode.com>
2021-09-26 08:50:03 -0700

    Removed FilterExtension

--- src/matching/Matching.Activities/Filter/ListFilterActivity.cs
+++ src/matching/Matching.Activities/Filter/ListFilterActivity.cs
@@ -1,48 +1,37 @@
 ﻿using GoodToCode.Analytics.Matching.Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GoodToCode.Analytics.Matching.Activities
 {
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        ListFilterActivity filterChain = new ListFilterActivity();
-    //        filterChain.ApplyFilter(4600);
-    //        filterChain.ApplyFilter(1900);
-    //        filterChain.ApplyFilter(600);
-    //    }
-    //}
-
-    public class ListFilterActivity
+    public class ListFilterActivity<T>
     {
-        private FilterExpression<object> filter = new FilterExpression<object>(x => x.ToString().Length > 0);
-        private ExpressionFilterHandler<object> firstHandler;
-        private ExpressionFilterHandler<object> secondHandler;
-        private ExpressionFilterHandler<object> thirdHandler;
+        public List<ExpressionFilterHandler<T>> Filters { get; }
+        public List<IEnumerable<T>> Results;
 
-        public ListFilterActivity()
+        public ListFilterActivity(IEnumerable<FilterExpression<T>> filters)
         {
-            firstHandler = new ExpressionFilterHandler<object>(filter);
-            secondHandler = new ExpressionFilterHandler<object>(filter);
-            thirdHandler = new ExpressionFilterHandler<object>(filter);
+            ExpressionFilterHandler<T> last = null;
+            foreach (var filter in filters)
+            {                
+                var next = new ExpressionFilterHandler<T>(filter);
+                Filters.Add(next);
+                if (last != null)
+                    last.SetNextHandler(next);
+                last = next;
+            }
+        }
+
+        public List<IEnumerable<T>> Execute(IEnumerable<T> listToFilter)
+        {
+            Filters.First().ApplyFilter(listToFilter);
             
-            // ToDo: Move to Parallel https://timdeschryver.dev/blog/process-your-list-in-parallel-to-make-it-faster-in-dotnet#parallel-linq-plinq
-            firstHandler.SetNextHandler(secondHandler);
-            secondHandler.SetNextHandler(thirdHandler);
-        }
-
-        public void Execute(IEnumerable<object> listToFilter)
-        {
-            // Start chain filters
-            firstHandler.ApplyFilter(listToFilter);
-
-            // Read results from each
-            var firstResults = firstHandler.FilteredList;
-            var secondResults = secondHandler.FilteredList;
-            var thirdResults = thirdHandler.FilteredList;
-
-        }
+            Results = new List<IEnumerable<T>>();
+            foreach (var filter in Filters)
+                Results.Add(filter.FilteredList);
+            
+            return Results;
+        }        
     }
 }
---
+++ src/matching/Matching.Activities/Filter/StringFilterActivity.cs
@@ -0,0 +1,52 @@
+﻿using GoodToCode.Analytics.Matching.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace GoodToCode.Analytics.Matching.Activities
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        ListFilterActivity filterChain = new ListFilterActivity();
+    //        filterChain.ApplyFilter(4600);
+    //        filterChain.ApplyFilter(1900);
+    //        filterChain.ApplyFilter(600);
+    //    }
+    //}
+
+    public class StringFilterActivity
+    {
+        // By colum type:
+        // Uri column: ContainsPath(), BeginsWithPath, EndsWithPath, Equals
+        // String column: Contains, BeginsWith, EndsWith, Equals
+
+        private FilterExpression<object> filter = new FilterExpression<object>(x => x.ToString().Length > 0);
+        private ExpressionFilterHandler<object> firstHandler;
+        private ExpressionFilterHandler<object> secondHandler;
+        private ExpressionFilterHandler<object> thirdHandler;
+
+        public StringFilterActivity()
+        {
+            firstHandler = new ExpressionFilterHandler<object>(filter);
+            secondHandler = new ExpressionFilterHandler<object>(filter);
+            thirdHandler = new ExpressionFilterHandler<object>(filter);
+            
+            // ToDo: Move to Parallel https://timdeschryver.dev/blog/process-your-list-in-parallel-to-make-it-faster-in-dotnet#parallel-linq-plinq
+            firstHandler.SetNextHandler(secondHandler);
+            secondHandler.SetNextHandler(thirdHandler);
+        }
+
+        public void Execute(IEnumerable<object> listToFilter)
+        {
+            // Start chain filters
+            firstHandler.ApplyFilter(listToFilter);
+
+            // Read results from each
+            var firstResults = firstHandler.FilteredList;
+            var secondResults = secondHandler.FilteredList;
+            var thirdResults = thirdHandler.FilteredList;
+
+        }
+    }
+}
