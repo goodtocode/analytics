5d2bb14011ed7456584f60cfc2f83bae02c498e8
Robert Good <robert.good@goodtocode.com>
2021-10-02 15:52:55 -0700

    Test created

--- src/matching/Matching.Unit.Tests/Filter/Filter_SequentialFilter_ActivityTests.cs
+++ src/matching/Matching.Unit.Tests/Filter/Filter_SequentialFilter_ActivityTests.cs
@@ -22,8 +22,7 @@ namespace GoodToCode.Analytics.Matching.Unit.Tests
         private readonly ExcelService excelService;
         private string SutOpinionFile { get { return @$"{PathFactory.GetProjectSubfolder("Assets")}/OpinionFile.xlsx"; } }
         private string SutDataSourceFile { get { return @$"{PathFactory.GetProjectSubfolder("Assets")}/Matching-DataSource-Small.xlsx"; } }
-        private string SutRuleFile { get { return @$"{PathFactory.GetProjectSubfolder("Assets")}/Matching-Rule-Map.xlsx"; } }
-        private string SutGoalFile { get { return @$"{PathFactory.GetProjectSubfolder("Assets")}/Matching-Goal-Categories.xlsx"; } }
+        private string SutRuleFile { get { return @$"{PathFactory.GetProjectSubfolder("Assets")}/Matching-Rule-Sequential.xlsx"; } }
         public RowEntity SutRow { get; private set; }
         public IEnumerable<ICellData> SutHeaders { get; private set; }
         public IEnumerable<FilterExpression<ICellData>> SutFilters { get; private set; }
@@ -60,13 +59,125 @@ namespace GoodToCode.Analytics.Matching.Unit.Tests
         }
 
         [TestMethod]
-        public async Task SequentialFilter_Activity_RuleFile()
+        public async Task SequentialFilter_Activity_Invalid()
         {
             Assert.IsTrue(File.Exists(SutDataSourceFile), $"{SutDataSourceFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
             Assert.IsTrue(File.Exists(SutRuleFile), $"{SutRuleFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
-            Assert.IsTrue(File.Exists(SutGoalFile), $"{SutGoalFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
 
-            SutFilters = new List<FilterExpression<ICellData>>() { new FilterExpression<ICellData>(x => x.ColumnIndex > -1) };
+            SutFilters = new List<FilterExpression<ICellData>>() { 
+                new FilterExpression<ICellData>(x => x.ColumnName == "Status Code" && x.CellValue != "OK"),
+                new FilterExpression<ICellData>(x => x.ColumnName == "Content Type" && x.CellValue != "text/html; charset=utf-8")
+            };
+
+            try
+            {
+                var bytes = await FileFactoryService.GetInstance().ReadAllBytesAsync(SutDataSourceFile);
+                Stream itemToAnalyze = new MemoryStream(bytes);
+                SutHeaders = excelService.GetSheet(itemToAnalyze, 0).ToSheetData().GetRow(1).Cells;
+                var workflow = new SequentialFilterActivity<ICellData>(SutFilters);
+                var results = workflow.Execute(SutHeaders);
+                Assert.IsTrue(results.Any(), "No results from filter service.");
+            }
+            catch (Exception ex)
+            {
+                logItem.LogError(ex.Message, ex);
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task SequentialFilter_Activity_ByAddressAndH2()
+        {
+            Assert.IsTrue(File.Exists(SutDataSourceFile), $"{SutDataSourceFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
+            Assert.IsTrue(File.Exists(SutRuleFile), $"{SutRuleFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
+
+            SutFilters = new List<FilterExpression<ICellData>>() { 
+                new FilterExpression<ICellData>(
+                    x => (x.ColumnName == "Address" && x.CellValue.Contains("/bulk-discounts"))
+                    && (x.ColumnName == "H2-1" && x.CellValue == "Certification Discounts"))
+            };
+
+            try
+            {
+                var bytes = await FileFactoryService.GetInstance().ReadAllBytesAsync(SutDataSourceFile);
+                Stream itemToAnalyze = new MemoryStream(bytes);
+                SutHeaders = excelService.GetSheet(itemToAnalyze, 0).ToSheetData().GetRow(1).Cells;
+                var workflow = new SequentialFilterActivity<ICellData>(SutFilters);
+                var results = workflow.Execute(SutHeaders);
+                Assert.IsTrue(results.Any(), "No results from filter service.");
+            }
+            catch (Exception ex)
+            {
+                logItem.LogError(ex.Message, ex);
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task SequentialFilter_Activity_ByAddressAndH1()
+        {
+            Assert.IsTrue(File.Exists(SutDataSourceFile), $"{SutDataSourceFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
+            Assert.IsTrue(File.Exists(SutRuleFile), $"{SutRuleFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
+
+            SutFilters = new List<FilterExpression<ICellData>>() { 
+                new FilterExpression<ICellData>(
+                    x => (x.ColumnName == "Address" && x.CellValue.Contains("/newsroom/clinical-voices"))
+                    && (x.ColumnName == "H1-1" && x.CellValue.StartsWith("Clinical Voices")))
+            };
+
+            try
+            {
+                var bytes = await FileFactoryService.GetInstance().ReadAllBytesAsync(SutDataSourceFile);
+                Stream itemToAnalyze = new MemoryStream(bytes);
+                SutHeaders = excelService.GetSheet(itemToAnalyze, 0).ToSheetData().GetRow(1).Cells;
+                var workflow = new SequentialFilterActivity<ICellData>(SutFilters);
+                var results = workflow.Execute(SutHeaders);
+                Assert.IsTrue(results.Any(), "No results from filter service.");
+            }
+            catch (Exception ex)
+            {
+                logItem.LogError(ex.Message, ex);
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task SequentialFilter_Activity_ByAddressAndTitle()
+        {
+            Assert.IsTrue(File.Exists(SutDataSourceFile), $"{SutDataSourceFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
+            Assert.IsTrue(File.Exists(SutRuleFile), $"{SutRuleFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
+
+            SutFilters = new List<FilterExpression<ICellData>>() { 
+                new FilterExpression<ICellData>(
+                    x => (x.ColumnName == "Address" && x.CellValue.Contains("/education/webinar-series"))
+                    && (x.ColumnName == "Title-1" && x.CellValue.StartsWith("Nurse Strong: Recognizing and Mitigating Moral Distress ")))
+            };
+
+            try
+            {
+                var bytes = await FileFactoryService.GetInstance().ReadAllBytesAsync(SutDataSourceFile);
+                Stream itemToAnalyze = new MemoryStream(bytes);
+                SutHeaders = excelService.GetSheet(itemToAnalyze, 0).ToSheetData().GetRow(1).Cells;
+                var workflow = new SequentialFilterActivity<ICellData>(SutFilters);
+                var results = workflow.Execute(SutHeaders);
+                Assert.IsTrue(results.Any(), "No results from filter service.");
+            }
+            catch (Exception ex)
+            {
+                logItem.LogError(ex.Message, ex);
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public async Task SequentialFilter_Activity_ByAddress()
+        {
+            Assert.IsTrue(File.Exists(SutDataSourceFile), $"{SutDataSourceFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
+            Assert.IsTrue(File.Exists(SutRuleFile), $"{SutRuleFile} does not exist. Executing: {Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}");
+
+            SutFilters = new List<FilterExpression<ICellData>>() { 
+                new FilterExpression<ICellData>(x => x.ColumnName == "Address" && x.CellValue != "/nursing-excellence/nurse-stories")
+            };
 
             try
             {
