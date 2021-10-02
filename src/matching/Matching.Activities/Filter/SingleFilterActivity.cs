d93050baee3426fab32725060683e246681c3381
Robert Good <robert.good@goodtocode.com>
2021-10-02 16:18:50 -0700

    NEw single handler

---
+++ src/matching/Matching.Activities/Filter/SingleFilterActivity.cs
@@ -0,0 +1,28 @@
+﻿using GoodToCode.Analytics.Matching.Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoodToCode.Analytics.Matching.Activities
+{
+    public class SingleFilterActivity<T>
+    {
+        public ExpressionFilterHandler<T> Filter { get; }
+        public List<IEnumerable<T>> Results;
+
+        public SingleFilterActivity(FilterExpression<T> filter)
+        {
+            Filter = filter;
+        }
+
+        public List<IEnumerable<T>> Execute(IEnumerable<T> listToFilter)
+        {
+            Filters.First().ApplyFilter(listToFilter);
+            
+            Results = new List<IEnumerable<T>>();
+            foreach (var filter in Filters)
+                Results.Add(filter.FilteredList);
+            
+            return Results;
+        }        
+    }
+}
---
+++ src/matching/Matching.Domain/Filter/SingleFilterHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoodToCode.Analytics.Matching.Domain
+{
+    public abstract class SingleFilterHandler<T> : IFilterHandler<T>
+    {
+        public SingleFilterHandler<T> NextHandler;
+        public IFilterExpression<T> Filter { get; }
+
+        public SingleFilterHandler(IFilterExpression<T> filter) { Filter = filter; }
+
+        public abstract IEnumerable<T> ApplyFilter(IEnumerable<T> filterableList);
+    }
+}
