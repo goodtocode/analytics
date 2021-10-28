e3cd5b447e352c8482dbdf46ae56ad14c3a91bc0
Robert Good <robert.good@goodtocode.com>
2021-10-28 16:31:18 -0700

    inc

--- src/abstractions/Analytics.Abstractions/Matching/DataSourceMatch.cs
+++ src/abstractions/Analytics.Abstractions/Matching/DataSourceMatch.cs
@@ -4,6 +4,10 @@ namespace GoodToCode.Analytics.Abstractions
 {
     public class DataSourceMatch<TDataSource> : IDataSourceMatch<TDataSource>
     {
+
+        public string matchedRuleRowKey { get; set; }
+        public string matchedDataRowKey { get; set; }
+
         public IEnumerable<FilterExpression<TDataSource>> MatchedRules { get; }
         public IEnumerable<TDataSource> MatchedData { get; }
 
