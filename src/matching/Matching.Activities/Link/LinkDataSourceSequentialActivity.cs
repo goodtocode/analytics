3a7ca186762cd87ee924d58c2a6b11248f5e3b4e
Robert Good <robert.good@goodtocode.com>
2021-11-18 09:26:17 -0800

    Simplified LinkDataSourceSequential.Execute to diagnose where stack overflow is coming from

--- src/matching/Matching.Activities/Link/LinkDataSourceSequentialActivity.cs
+++ src/matching/Matching.Activities/Link/LinkDataSourceSequentialActivity.cs
@@ -1,6 +1,7 @@
 ﻿using GoodToCode.Analytics.Abstractions;
 using GoodToCode.Analytics.Matching.Domain;
 using GoodToCode.Shared.Persistence.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -20,6 +21,7 @@ namespace GoodToCode.Analytics.Matching.Activities
             var currResults = new List<IMatchResultEntity<TDataSource>>();
             IEnumerable<TDataSource> filteredResults;
             IEnumerable<TDataSource> remainingDataSource = dataSource;
+            var matchedRowKeys = new List<string>();
 
             var rulePartitions = filterRules.GroupBy(r => r.PartitionKey);
             foreach (var partition in rulePartitions)
@@ -33,8 +35,11 @@ namespace GoodToCode.Analytics.Matching.Activities
                     else
                         filteredResults = new SingleFilterActivity<TDataSource>(expression.FirstOrDefault()).Execute(remainingDataSource);
                     foreach (var result in filteredResults)
+                    {
                         currResults.Add(new MatchResultEntity<TDataSource>(group.FirstOrDefault(), result));
-                    remainingDataSource = remainingDataSource.Where(c => filteredResults.Select(r => r.RowKey).Contains(c.RowKey) == false);
+                        matchedRowKeys.Add(result.RowKey);
+                    }
+                    remainingDataSource = remainingDataSource.Where(c => matchedRowKeys.Contains(c.RowKey) == false);
                 }
             }
 
