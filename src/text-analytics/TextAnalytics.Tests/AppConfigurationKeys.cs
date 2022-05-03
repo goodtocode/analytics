948a3a19178d5f756d6ab5937442967cecdea65c
Robert Good <robert.good@goodtocode.com>
2022-05-02 19:51:47 -0700

    refreshed pipelines

---
+++ src/text-analytics/TextAnalytics.Tests/AppConfigurationKeys.cs
@@ -0,0 +1,12 @@
+﻿namespace GoodToCode.Shared.TextAnalytics
+{
+    public struct AppConfigurationKeys
+    {
+        public const string SentinelKey = "Sentinel";
+        public const string CognitiveServicesEndpoint = "CognitiveServices:Endpoint";
+        public const string CognitiveServicesKeyCredential = "CognitiveServices:KeyCredential";
+        public const string TextAnalyticsSection = "TextAnalytics";
+        public const string TextAnalyticsEndpoint = "TextAnalytics:Endpoint";
+        public const string TextAnalyticsKeyCredential = "TextAnalytics:KeyCredential";
+    }
+}
